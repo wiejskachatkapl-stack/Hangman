aa698c8ce9ee1c0552c22e689589b0d58ec3b3c5
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-05-31 20:19:44 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const VERSION = 'WEB v1058';
+const VERSION = 'WEB v1059';
 const ALPHABET = 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ'.split('');
 const PHRASES = [
   {cat:'PAŃSTWO', text:'POLSKA'}, {cat:'PAŃSTWO', text:'JAPONIA'}, {cat:'PAŃSTWO', text:'TAJLANDIA'},
