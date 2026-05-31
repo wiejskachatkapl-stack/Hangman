f8083c39198a2f0d09cc3956d2f52caa7be111d1
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-05-31 18:33:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ function renderGallery(){const g=$('galleryBox'); g.innerHTML='';ZOMBIES.forEach
 // dla telefonu i komputera przy background-size: cover.
 const PLAY_BG_SIZE = { w: 2048, h: 1365 };
 const PLAY_HOTSPOTS = {
-single: { x: 70, y: 210, w: 500, h: 130 },  // GRA POJEDYNCZA
-multi:  { x: 70, y: 430, w: 500, h: 140},  // MULTIPLAYER
-back:   { x: 90, y: 560, w: 430, h: 120 }  // COFNIJ
+single: { x: 70, y: 400, w: 500, h: 130 },  // GRA POJEDYNCZA
+multi:  { x: 70, y: 630, w: 500, h: 140},  // MULTIPLAYER
+back:   { x: 90, y: 760, w: 430, h: 120 }  // COFNIJ
 };
 function placeHotspot(selector, box, rect, scale, ox, oy){
   const el = document.querySelector(selector);
