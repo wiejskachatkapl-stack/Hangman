efcfe1aa170dc6ec628f7078f5b1d1b7e75c143e
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-06-02 19:55:42 +0200

    Add files via upload

---
+++ assets/data/hasla.js
@@ -0,0 +1,62 @@
+window.ZH_PHRASES = [
+  {
+    "cat": "PAŃSTWO",
+    "text": "POLSKA"
+  },
+  {
+    "cat": "PAŃSTWO",
+    "text": "JAPONIA"
+  },
+  {
+    "cat": "PAŃSTWO",
+    "text": "TAJLANDIA"
+  },
+  {
+    "cat": "ZWIERZĘTA",
+    "text": "WILK"
+  },
+  {
+    "cat": "ZWIERZĘTA",
+    "text": "ŻÓŁW"
+  },
+  {
+    "cat": "ZWIERZĘTA",
+    "text": "NIETOPERZ"
+  },
+  {
+    "cat": "KUCHNIA",
+    "text": "PIEROGI"
+  },
+  {
+    "cat": "KUCHNIA",
+    "text": "ROSÓŁ"
+  },
+  {
+    "cat": "KUCHNIA",
+    "text": "ŻUREK"
+  },
+  {
+    "cat": "SPORT",
+    "text": "PIŁKA NOŻNA"
+  },
+  {
+    "cat": "SPORT",
+    "text": "KOSZYKÓWKA"
+  },
+  {
+    "cat": "KULTURA",
+    "text": "TEATR"
+  },
+  {
+    "cat": "MUZYKA",
+    "text": "GITARA"
+  },
+  {
+    "cat": "W DOMU",
+    "text": "LODÓWKA"
+  },
+  {
+    "cat": "ROŚLINY",
+    "text": "RÓŻA"
+  }
+];
